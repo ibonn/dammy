f3ce78a66ca74e506f2bbcb00af36efe0a712ec9
Ibon <ibonescartin@gmail.com>
2020-01-18 16:55:33 +0100

    Documentation improved

--- dammy/functions.py
+++ dammy/functions.py
@@ -7,6 +7,10 @@ from .core import FunctionResult
 def average(lst):
     """
     Get the average value of a list
+
+    :param lst: A list containing numeric values
+    :type lst: list
+    :returns: :class:`dammy.FunctionResult`
     """
     def avg(lst):
         return sum(lst) / len(lst)
@@ -15,19 +19,29 @@ def average(lst):
 def maximum(lst):
     """
     Get the maximum value of a list
+
+    :param lst: A list containing numeric values
+    :type lst: list
+    :returns: :class:`dammy.FunctionResult`
     """
     return FunctionResult(max, lst)
 
 def minimum(lst):
     """
     Get the minimum value of a list
+
+    :param lst: A list containing numeric values
+    :type lst: list
+    :returns: :class:`dammy.FunctionResult`
     """
     return FunctionResult(min, lst)
 
 def cast(obj, t):
     """
     Casts an object to the specified type
-    obj -> The object to be casted
-    t -> The type
+
+    :param obj: The object to cast
+    :param t: The type
+    :returns: :class:`dammy.FunctionResult`
     """
     return FunctionResult(t, obj)
