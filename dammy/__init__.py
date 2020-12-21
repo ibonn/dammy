49493309a4acaa26cb58d0d2600e190cff1f0552
Ibon <37736154+ibonn@users.noreply.github.com>
2020-12-21 18:41:05 +0100

    all modules added to dammy __init__

--- dammy/__init__.py
+++ dammy/__init__.py
@@ -3,4 +3,8 @@ dammy generates fake data, either to populate your databases with dummy data or 
 Datasets of any size can be easily generated and exported to SQL or as a dictionary.
 """
 
+__all__ = ('stdlib', 'db', 'exceptions', 'functions')
+
+from .core import seed
 from .core import BaseGenerator, EntityGenerator, FunctionResult, AttributeGetter, MethodCaller, OperationResult
+from .iterator import Iterator
